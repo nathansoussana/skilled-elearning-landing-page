aa276f505f6d814bc52b56f2361eec5fcdb540f9
Nathan Soussana <nathan.soussana@gmail.com>
2023-10-03 00:56:52 +0200

    Coded light and dark mode logo

--- src/jsx/Navbar.jsx
+++ src/jsx/Navbar.jsx
@@ -3,7 +3,8 @@ import {Button1} from './Buttons'
 export default function Navbar() {
   return (
     <nav>
-        <img src="logo-light.svg" alt="skilled logo" className="nav-logo" />
+        <img src="logo-dark.svg" alt="skilled logo" className="nav-logo nav-logo-light-mode" />
+        <img src="logo-light.svg" alt="skilled logo" className="nav-logo nav-logo-dark-mode" />
         <Button1 url="#" text="Get Started" />
     </nav>
   )
